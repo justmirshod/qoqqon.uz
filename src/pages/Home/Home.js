2278eaa53867516a1772f862ddcd792f2d9b1710
justmirshod <98507846+justmirshod@users.noreply.github.com>
2023-02-01 16:09:25 +0500

    `added navbar`

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,7 @@
+import { Navbar } from '../../components';
+
+const Home = () => {
+  return <Navbar />;
+};
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: '#e6e5e4',
+      },
+    },
   },
   plugins: [],
 };
