7f5ea9732c0b42c2e666776b85f8144a73d333de
dilshodsherzodovich <dilshod.sherzodovich.2004@gmail.com>
2023-02-23 17:41:54 +0500

    project finished

--- src/pages/Map/components/Information.js
+++ src/pages/Map/components/Information.js
@@ -1,23 +1,29 @@
+import sideInfo from '../../../translations/map.json';
+import { useSelector } from 'react-redux';
+
 const info = [
-  { name: "Maydon", data: "1940" },
-  { name: "Binolar", data: "3400" },
-  { name: "Kvartiralar", data: "24000" },
-  { name: "Ko'chalar", data: "1242" },
-  { name: "Aholi", data: "54000" },
-  { name: "Do'konlar", data: "1423" },
-  { name: "Ovqatlanish joylari", data: "1221" },
+  { name: 'Maydon', data: '1940' },
+  { name: 'Binolar', data: '3400' },
+  { name: 'Kvartiralar', data: '24000' },
+  { name: "Ko'chalar", data: '1242' },
+  { name: 'Aholi', data: '54000' },
+  { name: "Do'konlar", data: '1423' },
+  { name: 'Ovqatlanish joylari', data: '1221' },
 ];
 
 const Information = () => {
+  const { activeLang } = useSelector((state) => state.language);
   return (
     <div>
-      <h1 className="text-2xl font-semibold mb-5">Qo'qon tumani xaritasi</h1>
-      {info.map((item, index) => (
+      <h1 className='text-2xl font-semibold mb-5'>
+        {sideInfo.map.translations[activeLang]}
+      </h1>
+      {sideInfo.sideInfo.map((item, index) => (
         <div
-          className="flex items-center justify-between mb-3 pb-1 border-b-2 text-xl"
+          className='flex items-center justify-between mb-3 pb-1 border-b-2 text-xl'
           key={index}
         >
-          <h1 className="text-gray-500 ">{item.name}</h1>
+          <h1 className='text-gray-500 '>{item.translations[activeLang]}</h1>
           <span>{item.data}</span>
         </div>
       ))}
